b92ab4b7597ca82ab3e3bbd6eefc0ef74970ab92
Radhika1208 <radhikarkasat@gmail.com>
2021-10-11 19:41:00 +0530

    Photo management challenge

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -11,6 +11,7 @@ namespace API.Entities
         public string Url { get; set; }
 
         public bool IsMain { get; set; }
+        public bool IsApproved { get; set; }
 
         public string PublicId { get; set; }
 
